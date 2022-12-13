edbd66c81126836541bfb47da0769a1c6da4e780
LucaFaedda <lucakarl91@gmail.com>
2022-12-13 16:38:52 +0100

    aggiunta cartella bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,45 @@
+
+let team_array = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "Angela-Lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+
+]
+
+const listaTeam = document.querySelector('.lista-team')
+
+for(let i=0; i< team_array.length; i++){
+console.log(team_array[i])
+    
+    let team = team_array[i]
+    for(let key in team){
+        listaTeam.innerHTML += `<p class="text-white">${team[key]}</p>`
+    }
+}
